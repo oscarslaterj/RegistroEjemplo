25f0483c0350092b40d023b8333c0f1591efc8b4
Oscar Jimenez <oscar_jimenez@ucne.edu.do>
2018-09-03 19:14:24 -0400

    Creando clase persona

---
+++ RegistroEjemplo/Entidades/Persona.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegistroEjemplo.Entidades
+{
+   public class Persona
+    {
+        public int nombre { get; set; }
+    }
+}
--- RegistroEjemplo/Program.cs
+++ RegistroEjemplo/Program.cs
@@ -16,7 +16,7 @@ namespace RegistroEjemplo
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new RegistroEjemplo());
         }
     }
 }
