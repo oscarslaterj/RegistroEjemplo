a5e21455fff5fd127bfe014bedc3f5694d4a11f7
Oscar Jimenez <oscar_jimenez@ucne.edu.do>
2018-09-03 20:20:37 -0400

    Creando Contexto

---
+++ RegistroEjemplo/DAL/Scripts/Contexto.cs
@@ -0,0 +1,17 @@
+﻿using RegistroEjemplo.Entidades;
+using System.Data.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegistroEjemplo.DAL.Scripts
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Personas> Persona { get; set; }
+
+        public Contexto() : base("Constr") { }
+    }
+}
--- RegistroEjemplo/Entidades/Persona.cs
+++ RegistroEjemplo/Entidades/Persona.cs
@@ -1,13 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace RegistroEjemplo.Entidades
 {
-   public class Persona
+   public class Personas
     {
-        public int nombre { get; set; }
+        [Key]
+        public int PersonaID { get; set; }
+        public String Nombre { get; set; }
+        public String Telefono { get; set; }
+        public String Cedula { get; set; }
+        public String Direccion { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+
+        public Persona()
+        {
+            PersonaID = 0;
+            Nombre = string.Empty;
+            Telefono = string.Empty;
+            Cedula = string.Empty;
+            Direccion = string.Empty;
+            FechaNacimiento = DateTime.Now;
+        }
     }
 }
