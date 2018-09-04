dc754995a38e2d73d4a4a77c87973334f18d1b48
Oscar Slater Jimenez Paula <oscar_jimenez@ucne.edu.do>
2018-09-04 15:11:45 -0400

    Registro terminado, comenzando consulta

--- RegistroEjemplo/UI/Registros/RegistroEjemplo.Designer.cs
+++ RegistroEjemplo/UI/Registros/RegistroEjemplo.Designer.cs
@@ -30,7 +30,7 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RegistroEjemplo));
             this.label1 = new System.Windows.Forms.Label();
-            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.IDnumericUpDown = new System.Windows.Forms.NumericUpDown();
             this.Buscarbutton = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
@@ -39,13 +39,13 @@
             this.Nuevobutton = new System.Windows.Forms.Button();
             this.Guardarbutton = new System.Windows.Forms.Button();
             this.Eliminarbutton = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
-            this.maskedTextBox2 = new System.Windows.Forms.MaskedTextBox();
+            this.NombretextBox = new System.Windows.Forms.TextBox();
+            this.DirecciontextBox = new System.Windows.Forms.TextBox();
+            this.TelefonomaskedTextBox = new System.Windows.Forms.MaskedTextBox();
+            this.CedulamaskedTextBox = new System.Windows.Forms.MaskedTextBox();
             this.label6 = new System.Windows.Forms.Label();
-            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.FechaNacimientodateTimePicker = new System.Windows.Forms.DateTimePicker();
+            ((System.ComponentModel.ISupportInitialize)(this.IDnumericUpDown)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -57,23 +57,24 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "ID";
             // 
-            // numericUpDown1
+            // IDnumericUpDown
             // 
-            this.numericUpDown1.Location = new System.Drawing.Point(58, 30);
-            this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(70, 20);
-            this.numericUpDown1.TabIndex = 1;
+            this.IDnumericUpDown.Location = new System.Drawing.Point(72, 30);
+            this.IDnumericUpDown.Name = "IDnumericUpDown";
+            this.IDnumericUpDown.Size = new System.Drawing.Size(70, 20);
+            this.IDnumericUpDown.TabIndex = 1;
             // 
             // Buscarbutton
             // 
             this.Buscarbutton.Image = ((System.Drawing.Image)(resources.GetObject("Buscarbutton.Image")));
-            this.Buscarbutton.Location = new System.Drawing.Point(175, 18);
+            this.Buscarbutton.Location = new System.Drawing.Point(229, 12);
             this.Buscarbutton.Name = "Buscarbutton";
             this.Buscarbutton.Size = new System.Drawing.Size(105, 47);
             this.Buscarbutton.TabIndex = 2;
             this.Buscarbutton.Text = "Buscar";
             this.Buscarbutton.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
             this.Buscarbutton.UseVisualStyleBackColor = true;
+            this.Buscarbutton.Click += new System.EventHandler(this.Buscarbutton_Click);
             // 
             // label2
             // 
@@ -116,63 +117,66 @@
             this.Nuevobutton.Image = ((System.Drawing.Image)(resources.GetObject("Nuevobutton.Image")));
             this.Nuevobutton.Location = new System.Drawing.Point(12, 246);
             this.Nuevobutton.Name = "Nuevobutton";
-            this.Nuevobutton.Size = new System.Drawing.Size(103, 55);
+            this.Nuevobutton.Size = new System.Drawing.Size(92, 55);
             this.Nuevobutton.TabIndex = 7;
             this.Nuevobutton.Text = "Nuevo";
             this.Nuevobutton.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
             this.Nuevobutton.UseVisualStyleBackColor = true;
+            this.Nuevobutton.Click += new System.EventHandler(this.Nuevobutton_Click);
             // 
             // Guardarbutton
             // 
             this.Guardarbutton.Image = ((System.Drawing.Image)(resources.GetObject("Guardarbutton.Image")));
-            this.Guardarbutton.Location = new System.Drawing.Point(141, 246);
+            this.Guardarbutton.Location = new System.Drawing.Point(129, 246);
             this.Guardarbutton.Name = "Guardarbutton";
-            this.Guardarbutton.Size = new System.Drawing.Size(102, 55);
+            this.Guardarbutton.Size = new System.Drawing.Size(90, 55);
             this.Guardarbutton.TabIndex = 8;
             this.Guardarbutton.Text = "Guardar";
             this.Guardarbutton.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
             this.Guardarbutton.UseVisualStyleBackColor = true;
+            this.Guardarbutton.Click += new System.EventHandler(this.Guardarbutton_Click);
             // 
             // Eliminarbutton
             // 
             this.Eliminarbutton.Image = ((System.Drawing.Image)(resources.GetObject("Eliminarbutton.Image")));
-            this.Eliminarbutton.Location = new System.Drawing.Point(274, 246);
+            this.Eliminarbutton.Location = new System.Drawing.Point(241, 246);
             this.Eliminarbutton.Name = "Eliminarbutton";
-            this.Eliminarbutton.Size = new System.Drawing.Size(102, 55);
+            this.Eliminarbutton.Size = new System.Drawing.Size(94, 55);
             this.Eliminarbutton.TabIndex = 9;
             this.Eliminarbutton.Text = "Eliminar";
             this.Eliminarbutton.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
             this.Eliminarbutton.UseVisualStyleBackColor = true;
+            this.Eliminarbutton.Click += new System.EventHandler(this.Eliminarbutton_Click);
             // 
-            // textBox1
+            // NombretextBox
             // 
-            this.textBox1.Location = new System.Drawing.Point(72, 81);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(208, 20);
-            this.textBox1.TabIndex = 10;
+            this.NombretextBox.Location = new System.Drawing.Point(72, 81);
+            this.NombretextBox.Name = "NombretextBox";
+            this.NombretextBox.Size = new System.Drawing.Size(147, 20);
+            this.NombretextBox.TabIndex = 10;
             // 
-            // textBox2
+            // DirecciontextBox
             // 
-            this.textBox2.Location = new System.Drawing.Point(83, 179);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(239, 20);
-            this.textBox2.TabIndex = 11;
+            this.DirecciontextBox.Location = new System.Drawing.Point(72, 179);
+            this.DirecciontextBox.Name = "DirecciontextBox";
+            this.DirecciontextBox.Size = new System.Drawing.Size(147, 20);
+            this.DirecciontextBox.TabIndex = 11;
             // 
-            // maskedTextBox1
+            // TelefonomaskedTextBox
             // 
-            this.maskedTextBox1.Location = new System.Drawing.Point(72, 114);
-            this.maskedTextBox1.Mask = "(999)-999-9999";
-            this.maskedTextBox1.Name = "maskedTextBox1";
-            this.maskedTextBox1.Size = new System.Drawing.Size(100, 20);
-            this.maskedTextBox1.TabIndex = 12;
+            this.TelefonomaskedTextBox.Location = new System.Drawing.Point(72, 114);
+            this.TelefonomaskedTextBox.Mask = "(999)-999-9999";
+            this.TelefonomaskedTextBox.Name = "TelefonomaskedTextBox";
+            this.TelefonomaskedTextBox.Size = new System.Drawing.Size(147, 20);
+            this.TelefonomaskedTextBox.TabIndex = 12;
             // 
-            // maskedTextBox2
+            // CedulamaskedTextBox
             // 
-            this.maskedTextBox2.Location = new System.Drawing.Point(72, 148);
-            this.maskedTextBox2.Mask = "999-9999999-9";
-            this.maskedTextBox2.Name = "maskedTextBox2";
-            this.maskedTextBox2.Size = new System.Drawing.Size(100, 20);
-            this.maskedTextBox2.TabIndex = 13;
+            this.CedulamaskedTextBox.Location = new System.Drawing.Point(72, 148);
+            this.CedulamaskedTextBox.Mask = "999-9999999-9";
+            this.CedulamaskedTextBox.Name = "CedulamaskedTextBox";
+            this.CedulamaskedTextBox.Size = new System.Drawing.Size(147, 20);
+            this.CedulamaskedTextBox.TabIndex = 13;
             // 
             // label6
             // 
@@ -183,26 +187,26 @@
             this.label6.TabIndex = 14;
             this.label6.Text = "Fecha Nacimiento";
             // 
-            // dateTimePicker1
+            // FechaNacimientodateTimePicker
             // 
-            this.dateTimePicker1.CustomFormat = "dd/MM/yyyy";
-            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
-            this.dateTimePicker1.Location = new System.Drawing.Point(120, 212);
-            this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(99, 20);
-            this.dateTimePicker1.TabIndex = 15;
+            this.FechaNacimientodateTimePicker.CustomFormat = "dd/MM/yyyy";
+            this.FechaNacimientodateTimePicker.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.FechaNacimientodateTimePicker.Location = new System.Drawing.Point(120, 212);
+            this.FechaNacimientodateTimePicker.Name = "FechaNacimientodateTimePicker";
+            this.FechaNacimientodateTimePicker.Size = new System.Drawing.Size(99, 20);
+            this.FechaNacimientodateTimePicker.TabIndex = 15;
             // 
             // RegistroEjemplo
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(388, 313);
-            this.Controls.Add(this.dateTimePicker1);
+            this.ClientSize = new System.Drawing.Size(346, 313);
+            this.Controls.Add(this.FechaNacimientodateTimePicker);
             this.Controls.Add(this.label6);
-            this.Controls.Add(this.maskedTextBox2);
-            this.Controls.Add(this.maskedTextBox1);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.CedulamaskedTextBox);
+            this.Controls.Add(this.TelefonomaskedTextBox);
+            this.Controls.Add(this.DirecciontextBox);
+            this.Controls.Add(this.NombretextBox);
             this.Controls.Add(this.Eliminarbutton);
             this.Controls.Add(this.Guardarbutton);
             this.Controls.Add(this.Nuevobutton);
@@ -211,11 +215,12 @@
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.Buscarbutton);
-            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.IDnumericUpDown);
             this.Controls.Add(this.label1);
             this.Name = "RegistroEjemplo";
             this.Text = "Registro de Ejemplo";
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.Load += new System.EventHandler(this.RegistroEjemplo_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.IDnumericUpDown)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -224,7 +229,7 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown IDnumericUpDown;
         private System.Windows.Forms.Button Buscarbutton;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
@@ -233,12 +238,12 @@
         private System.Windows.Forms.Button Nuevobutton;
         private System.Windows.Forms.Button Guardarbutton;
         private System.Windows.Forms.Button Eliminarbutton;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
-        private System.Windows.Forms.MaskedTextBox maskedTextBox2;
+        private System.Windows.Forms.TextBox NombretextBox;
+        private System.Windows.Forms.TextBox DirecciontextBox;
+        private System.Windows.Forms.MaskedTextBox TelefonomaskedTextBox;
+        private System.Windows.Forms.MaskedTextBox CedulamaskedTextBox;
         private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker FechaNacimientodateTimePicker;
     }
 }
 
--- RegistroEjemplo/UI/Registros/RegistroEjemplo.cs
+++ RegistroEjemplo/UI/Registros/RegistroEjemplo.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using RegistroEjemplo.BLL;
+using RegistroEjemplo.Entidades;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +17,115 @@ namespace RegistroEjemplo
         public RegistroEjemplo()
         {
             InitializeComponent();
+        }
+
+        private void Limpiar()
+        {
+            IDnumericUpDown.Value = 0;
+            NombretextBox.Text = string.Empty;
+            CedulamaskedTextBox.Text = string.Empty;
+            TelefonomaskedTextBox.Text = string.Empty;
+            DirecciontextBox.Text = string.Empty;
+            FechaNacimientodateTimePicker.Value = DateTime.Now;
+           // SuperErrorProvider.Clear();
+        }
+
+        private void RegistroEjemplo_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void Nuevobutton_Click(object sender, EventArgs e)
+        {
+            Limpiar();
+        }
+
+        private bool ExisteEnLaBaseDeDatos()
+        {
+            Personas persona = PersonaBLL.Buscar((int)IDnumericUpDown.Value);
+            return (persona != null);
+        }
+
+        private Personas Llenaclase()
+        {
+            Personas persona = new Personas();
+            persona.PersonaID = Convert.ToInt32(IDnumericUpDown.Value);
+            persona.Nombre = NombretextBox.Text;
+            persona.Cedula = CedulamaskedTextBox.Text;
+            persona.Telefono = TelefonomaskedTextBox.Text;
+            persona.Direccion = DirecciontextBox.Text;
+            persona.FechaNacimiento = FechaNacimientodateTimePicker.Value;
+            return persona;
+        }
+
+        private void Guardarbutton_Click(object sender, EventArgs e)
+        {
+            Personas persona;
+            bool paso = false;
+
+            /* if (!Validar())
+             return;*/
+
+            persona = Llenaclase();
+
+            if(IDnumericUpDown.Value == 0)
+              paso = PersonaBLL.Guardar(persona);
+            else
+            {
+                if(!ExisteEnLaBaseDeDatos())
+                {
+                    MessageBox.Show("No se puede modificar una persona que no existe", "Fallo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }paso = PersonaBLL.Modificar(persona);
+
+            }Limpiar();
+
+            if(paso)
+           
+                MessageBox.Show("Guardado!!", "Exito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                else
+                MessageBox.Show("No se puedo guardar", "Error", MessageBoxButtons.OK, MessageBoxIcon.Information); 
+        }
+
+        private void LlenaCampo(Personas persona)
+        {
+            int personaID = new Personas().PersonaID;
+            IDnumericUpDown.Value = personaID;
+            NombretextBox.Text = new Personas().Nombre;
+            TelefonomaskedTextBox.Text = new Personas().Telefono;
+            CedulamaskedTextBox.Text = new Personas().Telefono;
+            DirecciontextBox.Text = new Personas().Direccion;
+            FechaNacimientodateTimePicker.Value = new Personas().FechaNacimiento;
+        }
+
+        private void Buscarbutton_Click(object sender, EventArgs e)
+        {
+            int id;
+            Personas persona = new Personas();
+            int.TryParse(IDnumericUpDown.Text, out id);
+
+            persona = PersonaBLL.Buscar(id);
+
+            if(persona != null)
+            {
+                MessageBox.Show("Persona Encontrada");
+                LlenaCampo(persona);
+            }
+            else
+            {
+                MessageBox.Show("Persona no encontrada");
+            }
+        }
+
+        private void Eliminarbutton_Click(object sender, EventArgs e)
+        {
+            int id;
+            int.TryParse(IDnumericUpDown.Text, out id);
+
+            if (PersonaBLL.Eliminar(id))
+                MessageBox.Show("Eliminado");
+            else
+                ErrorProvider.Equals(IDnumericUpDown, "No se puede eliminar una persona que no existe");
         }
     }
 }
